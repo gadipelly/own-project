a8ba5e723e62ac48de5fa42e67ea9f931a02bbaf
gadipellyhrushikesh363 <gadipellyhrushikesh363@gmail.com>
2024-10-22 12:41:17 +0530

    create login components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignInComponent } from './Login/sign-in/sign-in.component';
+import { RegisterComponent } from './Login/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'sign-in', component:SignInComponent},
+  {path:'register',component:RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './Login/sign-in/sign-in.component';
+import { RegisterComponent } from './Login/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
